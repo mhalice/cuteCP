5fc8713101a519b397193156cca551afa5907458
Matheus Macabu <mkbu95@gmail.com>
2013-02-06 23:49:24 -0200

    * Corrigido alguns encodings.
    Signed-off-by: Matheus Macabu <mkbu95@gmail.com>

--- modules/characters.php
+++ modules/characters.php
@@ -1,9 +1,9 @@
 <?php
 /*
- * Este sistema web é protegido por leis internacionais 
+ * Este sistema web Ã© protegido por leis internacionais 
  * e pela lei de Deus.
  *
- * Afinal de contas, só Deus sabe como essa merda funciona..
+ * Afinal de contas, sÃ³ Deus sabe como essa merda funciona..
  * -------------
  * desenvolvido por eru yuuko
  */
